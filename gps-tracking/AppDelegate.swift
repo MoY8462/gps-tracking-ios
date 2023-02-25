ae472c9cabe422ee48e514629f88f7959d678cf5
BetoNajera <ing.betonajera@gmail.com>
2023-02-25 01:27:28 -0600

    feat: :sparkles: add map to home view controller

--- gps-tracking/AppDelegate.swift
+++ gps-tracking/AppDelegate.swift
@@ -7,7 +7,7 @@
 
 import UIKit
 import FirebaseCore
-
+import GoogleMaps
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
@@ -16,6 +16,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
         FirebaseApp.configure()
+        GMSServices.provideAPIKey("AIzaSyA6DmttXrKn0GbzQF6LCntFi37fQ_OF-ho")
         return true
     }
 
--- gps-tracking/HomeViewController.swift
+++ gps-tracking/HomeViewController.swift
@@ -4,27 +4,45 @@
 //
 //  Created by Moises Sancheza on 22/02/23.
 //
-
+import GoogleMaps
 import UIKit
 
 enum ProviderType: String {
     case basic
 }
 
-class HomeViewController: UIViewController {
+class HomeViewController: UIViewController, GMSMapViewDelegate {
 
     @IBOutlet var emailLabel: UILabel!
     @IBOutlet var providerLabel: UILabel!
     @IBOutlet var closeSessionButton: UIButton!
+    private var mapView: GMSMapView!
     var email: String?
-    
-    
+    override func loadView() {
+
+        // Load the map at set latitude/longitude and zoom level
+        let camera:GMSCameraPosition = GMSCameraPosition.camera(withLatitude: 19.3898, longitude: -99.178, zoom: 6.0)
+
+        mapView = GMSMapView(frame: .zero, camera: camera)
+        mapView.settings.compassButton = true
+        mapView.settings.myLocationButton = true
+        mapView.settings.scrollGestures = true
+          mapView.settings.zoomGestures = true
+        self.view = mapView
+        mapView.delegate = self
+      }
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        title = "Inicio"
+        title = "Mapa"
         // Do any additional setup after loading the view.
-        emailLabel.text = email
+        let mapCenter = CLLocationCoordinate2DMake(19.3898,-99.178)
+        let marker = GMSMarker(position: mapCenter)
+        marker.icon = UIImage(named: "custom_pin.png")
+        marker.title = "Center CDMX"
+        marker.map = mapView
+        
+        
     }
     
     
